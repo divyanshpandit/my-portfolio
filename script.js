d5a5cea26cf3f9993f7bcdcf873a1df2df036cb7
Divyanshu dwivedi <divyanshpandiit@gmail.com>
2025-06-20 15:25:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const sections = document.querySelectorAll("section");
+const typedText = "Welcome to my Pythonic Portfolio...";
+let index = 0;
+const speed = 100;
 
+function type() {
+  if(index < typedText.length){
+    document.getElementById("typed").innerHTML += typedText.charAt(index);
+    index++;
+    setTimeout(type, speed);
+  }
+}
+window.onload = type;
+
+const sections = document.querySelectorAll("section");
 function checkVisibility() {
   sections.forEach(section => {
     const top = section.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
+    if (top < window.innerHeight - 50) {
       section.classList.add("visible");
     }
   });
 }
-
 window.addEventListener("scroll", checkVisibility);
 window.addEventListener("load", checkVisibility);
